337aaa66623d55ba2299f1385d9bef33f30e9c77
noemi <gynoemi33@gmail.com>
2021-02-11 15:28:12 +0100

    Create connection with the streamer

---
+++ server.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const io = require("socket.io-client");
+
+const port = 4000 || process.env.PORT;
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+const server = http.createServer(app);
+
+const clientSocket = io.connect("http://localhost:5000", {
+  withCredentials: true,
+});
+clientSocket.on("message", (message) => {
+  console.log("Server: " + message);
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
